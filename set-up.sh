865f0f079187d422730d1cac1f65b7edeafdaa7e
Liam Bindle <liambindle@gmail.com>
2020-02-20 14:29:31 -0600

    updated names

--- set-up.sh
+++ set-up.sh
@@ -18,17 +18,17 @@ if [ $? != 0 ]; then
  exit 1
 fi
 
-# Download head-node.rc and interactive.bsub
-wget -qO- https://raw.githubusercontent.com/Atmospheric-Composition-Analysis-Group/compute1/master/head-node.rc | sed "s#MY_PROJECTS_DIR=[a-zA-Z0-9_-\./]*#MY_PROJECTS_DIR=$MY_PROJECTS_DIR#g" > head-node.rc
+# Download lsf-conf.rc and interactive.bsub
+wget -qO- https://raw.githubusercontent.com/Atmospheric-Composition-Analysis-Group/compute1/master/lsf-conf.rc | sed "s#MY_PROJECTS_DIR=[a-zA-Z0-9_-\./]*#MY_PROJECTS_DIR=$MY_PROJECTS_DIR#g" > lsf-conf.rc
 wget -q https://raw.githubusercontent.com/Atmospheric-Composition-Analysis-Group/compute1/master/interactive.bsub
 wget -q https://raw.githubusercontent.com/Atmospheric-Composition-Analysis-Group/compute1/master/.zshrc
 
-# Modify ~/.bashrc to load head-node.rc
-if ! $(grep -q '# Source ~/head-node.rc if we are on the head-node' ~/.bashrc); then
+# Modify ~/.bashrc to load lsf-conf.rc
+if ! $(grep -q '# Source ~/lsf-conf.rc if we are on the head-node' ~/.bashrc); then
     echo '
-# Source ~/head-node.rc if we are on the head-node
+# Source ~/lsf-conf.rc if we are on the head-node
 if $(hostname | grep -q "compute1-client"); then
-    source $HOME/head-node.rc
+    source $HOME/lsf-conf.rc
 fi
 ' >> ~/.bashrc
 fi
